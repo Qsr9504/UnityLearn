11508d4da0be14eefb2e0852cc9972b5fbb85257
Qsr9504 <qsr9504@outlook.com>
2018-06-19 09:53:18 +0800

    c#与unity

---
+++ Assets/Scripts/InspectorViewDemo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//定义一个类
+[System.Serializable]
+public class customStruct {
+    public int mm;
+    public string qsr;
+}
+public class InspectorViewDemo : MonoBehaviour {
+    
+    //public属性会被自动暴露在unity里
+    //首字母默认会被转化为大写
+    //unity中赋值会将代码中的初始值给覆盖掉
+    public int ab = 2;//定义一个int型
+    public bool[] booldemo;//定义一个布尔型数组，不用担心空指针
+    public List<string> ayyayList;//定义一个string数组
+
+    public customStruct custom;
+
+
+    public Color color;//定义一个颜色
+    public AnimationCurve curve;//定义一个曲线
+
+    public Gradient gradient;//是一个渐变（颜色）
+
+    //protected,private 都是不会被暴露出来的
+    private int a;
+    protected int b;
+    
+    //将public的隐藏起来
+    [HideInInspector]
+    public int bb = 2;
+
+    //将private 和 protected 的暴露出来
+    [SerializeField]
+    private int cc;
+    [SerializeField]
+    protected int dd;
+
+	void Start () {
+        Debug.Log(ab);//打印ab的值
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MyCompoment1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCompoment1 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        //动态在代码中挂载一个Compoment
+        MyCompoment2 my2 = gameObject.AddComponent<MyCompoment2>();
+        //寻找某一个脚本,或者一个组件，需要进行判断是否为空
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        //Renderer renderer1 = gameObject.GetComponentInChildren<Renderer>();//找到下属的第一个层级的
+
+        //删除某一个脚本
+        Destroy(renderer);//需要先找到这个脚本
+
+        //禁用一个脚本
+        my2.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void Awake() {
+        //相当于构造函数，这个脚本第一次初始化的
+    }
+
+    private void OnEnable() {
+        //当每次被启用的时候调用的函数
+
+        //onStart之前会调用一下这个函数
+    }
+
+    private void OnDisable() {
+        //当这个脚本每次被禁用的时候调用的函数
+        
+        //再被摧毁之前每次都会被调用
+    }
+
+     
+}

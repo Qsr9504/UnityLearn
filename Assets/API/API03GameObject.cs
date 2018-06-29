05785ba4d22e49838cfd9828b3a3c53aea864c70
Qsr9504 <qsr9504@outlook.com>
2018-06-29 09:52:49 +0800

    API学习
    创建GameObject的三种方法

---
+++ Assets/API/API03GameObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class API03GameObject : MonoBehaviour {
+    public GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        //1.第一种创建方法
+        //GameObject gameObject = new GameObject("Cube");
+        //2.第二种创建方法  根据prefab进行创建的
+        //根据另外的游戏物体进行克隆
+        GameObject.Instantiate(prefab);
+        //3.第三种创建方法
+        GameObject.CreatePrimitive(PrimitiveType.Plane);
+        GameObject game = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        
+        
+        //为已有物体进行添加组件
+        game.AddComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
